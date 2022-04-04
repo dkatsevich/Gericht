0ce0f2879a445ff7ceb0168cdf3a5bfd4c86daa0
dkatsevich <d.katsevich.4@gmail.com>
2022-04-04 15:54:00 +0300

    start work for mainslider

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -1,6 +1,11 @@
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
+// import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+// import { flsModules } from "./modules.js";
+
+
+window.addEventListener('load', (e) => {
+    document.documentElement.insertAdjacentHTML('beforeend', `<div class="main-bg"></div>`)
+})
 
 
